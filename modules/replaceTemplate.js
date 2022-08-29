f352e68d8513cd706d4c39f17d1f2b2c37cb517b
moeeahmed <m.ahamedbusiness@gmail.com>
2022-08-29 13:48:37 +0100

    Modules include function to replace HTML

---
+++ modules/replaceTemplate.js
@@ -0,0 +1,31 @@
+module.exports = (temp, data) => {
+  const direction = {
+    N: '↑',
+    NE: '↗',
+    E: '→',
+    SE: '↘',
+    S: '↓',
+    SW: '↙',
+    W: '←',
+    NW: '↖',
+    undefined: '',
+  };
+
+  let output = temp.replace(/%COUNTRY_NAME%/g, data.location.country);
+  output = output.replace(/%CITY_NAME%/g, data.location.name);
+  output = output.replace(/%CONDITION%/g, data.current.condition.text);
+  output = output.replace(/%WIND_SPEED%/g, data.current.wind_mph);
+  output = output.replace(
+    /%WIND_DIR%/g,
+    `${data.current.wind_dir} ${
+      direction[data.current?.wind_dir] === undefined
+        ? ''
+        : direction[data.current?.wind_dir]
+    }`
+  );
+  output = output.replace(/%PRECIP%/g, data.current.precip_mm);
+  output = output.replace(/%IMG_SRC%/g, data.current.condition.icon);
+  output = output.replace(/%TEMPERATURE%/g, Math.round(data.current.temp_c));
+
+  return output;
+};
