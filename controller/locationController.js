f7a1daffd0600431a81f5e23b0263ee118d1c936
moeeahmed <m.ahamedbusiness@gmail.com>
2022-09-02 13:51:11 +0100

    added request for 3 day forecast

--- controller/locationController.js
+++ controller/locationController.js
@@ -80,3 +80,32 @@ exports.getSearch = (req, res) => {
 exports.redirect = (_, res) => {
   res.redirect('/location');
 };
+
+// const http = require("https");
+
+// const options = {
+// 	"method": "GET",
+// 	"hostname": "weatherapi-com.p.rapidapi.com",
+// 	"port": null,
+// 	"path": "/forecast.json?q=London&days=3",
+// 	"headers": {
+// 		"X-RapidAPI-Key": "6d82f956bamsh079341077b9577ep15f139jsnb77afa8ba2cf",
+// 		"X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+// 		"useQueryString": true
+// 	}
+// };
+
+// const req = http.request(options, function (res) {
+// 	const chunks = [];
+
+// 	res.on("data", function (chunk) {
+// 		chunks.push(chunk);
+// 	});
+
+// 	res.on("end", function () {
+// 		const body = Buffer.concat(chunks);
+// 		console.log(body.toString());
+// 	});
+// });
+
+// req.end();
