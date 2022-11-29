bc862d3c9ba010ce33b68553ef49011a01e473e9
moeeahmed <m.ahamedbusiness@gmail.com>
2022-11-29 15:46:17 +0000

    added dotenv and pug templates route

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict';
-
-const morgan = require('morgan');
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -11,7 +10,10 @@ const locationRouter = require('./routes/locationRoutes');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-// app.use(morgan('dev'));
+
+//view engine
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 // -- ROUTES --
 
